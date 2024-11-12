454b6e0bb2c7c74ac23ff96604999450bcd47bf3
bri.depinto <briannadepinto@gmail.com>
2024-11-12 10:58:40 -0500

    Implement AJAX request to load JSON data

--- gallery.js
+++ gallery.js
@@ -23,7 +23,17 @@ $(document).ready(() => {
 // Function to fetch JSON data and store it in mImages
 function fetchJSON () {
   // Use $.ajax here to request the JSON data from mUrl
-  // On success, parse the JSON and push each image object into mImages array
+  $.ajax({
+    type: 'GET',
+    url: mUrl,
+    // On success, parse the JSON and push each image object into mImages array
+    success: function (data) {
+      
+    },
+    error: function () {
+      console.log('Connection error.');
+    }
+  });
   // After JSON is loaded, call swapPhoto() to display the first image
 }
 
